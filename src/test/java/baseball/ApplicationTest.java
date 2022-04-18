330296b5903afc30fd396be6c9668877605b0de8
soob-forest <dltntnqo@naver.com>
2022-04-18 21:01:46 +0900

    test(ApplicationTest): 3자리 보다 작은 경우 테스트 케이스 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -23,7 +23,17 @@ class ApplicationTest extends NsTest {
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("12"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+
+        assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("1234"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("a"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
 
